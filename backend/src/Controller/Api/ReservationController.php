e441250042552038cbd85bae58143471a5824367
Artchibo7 <arthur.zulpukharov.simplon@gmail.com>
2024-05-16 11:59:48 +0200

    ajout reservation

--- backend/src/Controller/Api/ReservationController.php
+++ backend/src/Controller/Api/ReservationController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Api;
 
 use App\Entity\Reservation;
 use App\Entity\Statut;
+use App\Entity\Voyage;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -15,14 +16,13 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Route('/api/reservation', name: 'api_reservation_')]
 class ReservationController extends AbstractController
-{
-    #[Route('/new', name: "new", methods: ['POST'])]
+{#[Route('/new', name: "new", methods: ['POST'])]
     public function new(Request $request, EntityManagerInterface $em, SerializerInterface $serializer, ValidatorInterface $validator): Response
     {
         $requestData = json_decode($request->getContent(), true);
         $statutId = $requestData['statut']['id'];
         $voyageID = $requestData['voyage']['id'];
-
+    
         $statut = $em->getRepository(Statut::class)->find($statutId);
         if (!$statut) {
             return new JsonResponse(['message' => 'Invalid statut ID provided'], Response::HTTP_BAD_REQUEST);
@@ -31,29 +31,31 @@ class ReservationController extends AbstractController
         if (!$voyage) {
             return new JsonResponse(['message' => 'Invalid voyage ID provided'], Response::HTTP_BAD_REQUEST);
         }
-
+    
         $reservation = $serializer->deserialize($request->getContent(), Reservation::class, 'json', ['groups' => 'api_reservation_new']);
         $reservation->setStatut($statut);
-
+        $reservation->setVoyage($voyage); // Set the related voyage
+    
         $reservation->setNom($requestData['nom']);
         $reservation->setPrenom($requestData['prenom']);
         $reservation->setEmail($requestData['email']);
         $reservation->setTelephone($requestData['telephone']);
-
+    
         $errors = $validator->validate($reservation);
-
+    
         if (count($errors) > 0) {
             $errorMessages = [];
             foreach ($errors as $error) {
                 $errorMessages[] = $error->getMessage();
             }
-
+    
             return new JsonResponse($errorMessages, Response::HTTP_UNPROCESSABLE_ENTITY);
         } else {
             $em->persist($reservation);
             $em->flush();
-
+    
             return new JsonResponse(['message' => 'Le reservation a bien été envoye'], Response::HTTP_CREATED);
         }
     }
+    
 }
